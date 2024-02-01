0ee162edd6913de60583ea8fa434ca4e1cc706c1
rashadnassar27 <rashad.seo2@gmail.com>
2024-02-01 23:27:38 +0200

    .

--- src/Bot.js
+++ src/Bot.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import CallButton from './CallButton'; // Adjust the path accordingly
-import './Bot.css'; // Import the CSS file for styling
+import React, { useState } from "react";
+import CallButton from "./CallButton"; // Adjust the path accordingly
+import "./Bot.css"; // Import the CSS file for styling
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import epaperlogo from "./assets/images/epaper.png";
 
 const Bot = () => {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+
+  const sendMessage = () => {
+    // setMessages([...messages, { text: input, sender: "me" }]);
+    // setInput("");
+  };
+
   return (
-    <div className="bot-container">
-      <div className="top-bar">
-        <h2>ePaper Support</h2>
-        <CallButton />
+    <div className="chat-page">
+      <div className="chat-header">
+        <div className="logo-container">
+          <epaperlogo />
+        </div>
+
+        <div className="call-button-container">
+          <CallButton />
+        </div>
       </div>
-      <div className="chat-container">
-        {/* Chat messages go here */}
+      <div className="chat-messages">
+        {messages.map((message, index) => (
+          <div key={index} className={`chat-message ${message.sender}`}>
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+        />
+        <button className="send-button" onClick={sendMessage}>
+          <FontAwesomeIcon icon={faPaperPlane} size="2x" />
+        </button>
       </div>
     </div>
   );
