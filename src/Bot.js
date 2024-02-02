821580c69844379e8cd18009f9c68916a372f5d7
rashadnassar27 <rashad.seo2@gmail.com>
2024-02-02 10:42:44 +0200

    .

--- src/Bot.js
+++ src/Bot.js
@@ -4,6 +4,7 @@ import "./Bot.css"; // Import the CSS file for styling
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import epaperlogo from "./assets/images/epaper.png";
+import { Helmet } from "react-helmet";
 
 const Bot = () => {
   const [messages, setMessages] = useState([]);
@@ -15,34 +16,40 @@ const Bot = () => {
   };
 
   return (
-    <div className="chat-page">
-      <div className="chat-header">
-        <div className="logo-container">
-          <epaperlogo />
-        </div>
-
-        <div className="call-button-container">
-          <CallButton />
-        </div>
-      </div>
-      <div className="chat-messages">
-        {messages.map((message, index) => (
-          <div key={index} className={`chat-message ${message.sender}`}>
-            {message.text}
+    <>
+      {" "}
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
+      <div className="chat-page">
+        <div className="chat-header">
+          <div className="logo-container">
+            <epaperlogo />
           </div>
-        ))}
+
+          <div className="call-button-container">
+            <CallButton />
+          </div>
+        </div>
+        <div className="chat-messages">
+          {messages.map((message, index) => (
+            <div key={index} className={`chat-message ${message.sender}`}>
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <div className="chat-input">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+          />
+          <button className="send-button" onClick={sendMessage}>
+            <FontAwesomeIcon icon={faPaperPlane} size="2x" />
+          </button>
+        </div>
       </div>
-      <div className="chat-input">
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          type="text"
-        />
-        <button className="send-button" onClick={sendMessage}>
-          <FontAwesomeIcon icon={faPaperPlane} size="2x" />
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
